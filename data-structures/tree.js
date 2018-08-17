5fb832d9ad8df9928ef1d351ce2e5263eef211b6
Manuel Jasso <mjasso@stubhub.com>
2018-08-17 09:53:49 -0700

    My solution for Tree

--- data-structures/tree.js
+++ data-structures/tree.js
@@ -39,29 +39,117 @@ https://en.wikipedia.org/wiki/Trie
 
 */
 
-function Tree (value) {
-  // implement me...
+function Tree (value, parent) {
+  this.value = value;
+  this.parent = parent;
+  this.children = [];
 }
 
 Tree.prototype.addChild = function(value) {
-  // implement me...
+  let newTree = new Tree(value, this);
+  this.children.push(newTree);
+  return newTree;
 };
-// Time complexity:
-
 
 Tree.prototype.contains = function(value) {
-  // implement me...
+  let found = false;
+  this.traverseDepthFirst((nodeValue) => {
+    console.log(`nodeValue:${nodeValue}`);
+    if (nodeValue === value) {
+      found = true;
+      return false; // @ToDo: Fix so that it won't continue
+    } else {
+      return true;
+    }
+  });
+  return found;
 };
-// Time complexity:
 
+Tree.prototype.print = function (traverseFn) {
+  traverseFn(console.log);
+}
 
 Tree.prototype.traverseDepthFirst = function(fn) {
-  // implement me...
+  let cont = fn(this.value);
+  console.log(`cont:${cont}`);
+  if (cont) { // @ToDo: Fix so that it won't continue
+    for (let i = 0, len = this.children.length; i < len; i++) {
+      if (this.children[i]) {
+        this.children[i].traverseDepthFirst(fn)
+      }
+    }
+  };
 };
-// Time complexity:
-
 
 Tree.prototype.traverseBreadthFirst = function(fn) {
-  // implement me...
+
+  function height (tree) {
+    if (tree.children.length === 0) {
+      // console.log(`height:${tree.value}=1`);
+      return 1;
+    } else {
+      let hmax = 0;
+      tree.children.forEach(child => {
+        hc = height(child);
+        if (hc > hmax) {
+          hmax = hc;
+        }
+      });
+      // console.log(`height:${tree.value}=${hmax + 1}`);
+      return hmax + 1;
+    }
+  }
+
+  function traverseLevel (tree, level) {
+    if (level >= 1 && level <= treeHeight) {
+      if (level === 1) {
+        // console.log(`${level}:${tree.value} *`);
+        fn(tree.value);
+      } else {
+        // console.log(`${level}:${tree.value} >`);
+        tree.children.forEach(child => {
+          // console.log(`${level-1}:${child.value} .`);
+          traverseLevel(child, level-1);
+        });
+      }
+    }
+  }
+
+  let treeHeight = height(this);
+
+  for (let level = 1; level <= treeHeight; level++) {
+    traverseLevel(this,level);
+  }
 };
-// Time complexity:
+
+// http://www.cse.unsw.edu.au/~billw/Justsearch1.gif
+
+let a = new Tree('A');
+
+let b = a.addChild('B');
+let c = a.addChild('C');
+
+let d = b.addChild('D');
+let e = b.addChild('E');
+
+let f = c.addChild('F');
+let g = c.addChild('G');
+
+let h = d.addChild('H');
+
+let i = e.addChild('I');
+let j = e.addChild('J');
+
+let k = f.addChild('K');
+
+// let l = k.addChild('L');
+
+// let m = l.addChild('M');
+
+// console.log('Breath First:')
+// a.print(a.traverseBreadthFirst.bind(a));
+
+// console.log('Depth First:')
+// a.print(a.traverseDepthFirst.bind(a));
+
+console.log(a.contains('J'));
